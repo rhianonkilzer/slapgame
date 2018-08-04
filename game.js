9e0a5145efa1a3179eef5ca5178086ca631c2d23
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-04 14:19:43 -0600

    commit

--- game.js
+++ game.js
@@ -1,40 +1,95 @@
-let health = 100;
-let hits = 0;
+let zombie = {
+    health: 100,
+    maxHealth: 100,
+    hits: 0,
+    items: []
+}
 
+let availableItems = {
+    shield: {
+        name: 'half damage',
+        modifier: .50,
+        description: 'does half the damage'
+    },
+    brains: {
+        name: 'adds health',
+        modifier: 0,
+        description: 'yummmm'
+    },
+    dodge: {
+        name: 'dodge',
+        modifier: 1,
+        description: 'dodges an attack'
+    }
+}
 
 function punch() {
-    health -= 3;
-    hits++
+    zombie.health -= 3 * addMods();
+    zombie.hits++
     update()
 }
 
 function gassy() {
-    health -= 13;
-    hits++
+    zombie.health -= 13 * addMods();
+    zombie.hits++
     update()
 }
 
 function stabby() {
-    health -= 20;
-    hits++
+    zombie.health -= 20 * addMods();
+    zombie.hits++
     update()
 }
 
+function addMods() {
+    let totalMods = 1
+    for(let index = 0; index < zombie.items.length; index++){
+        let item = zombie.items[index]
+        totalMods -= item.modifier
+    }
+    if(totalMods < 0){
+        totalMods = 0;
+    }
+    zombie.items = []
+    return totalMods
+}
+ 
 
-function reset(){
-    health = 100;
-    hits = 0;
+function shield() {
+    zombie.items.push(availableItems.shield)
+
+
+}
+
+function dodge() {
+    zombie.items.push(availableItems.dodge)
+
+
+}
+
+function brains() {
+    zombie.items.push(availableItems.brains)
+    zombie.health += 10;
+    update()
+}
+
+function reset() {
+    zombie.health = 100;
+    zombie.hits = 0;
     update()
 }
 
 function update() {
-    if(health < 0){
-    health = 0;
+    if (zombie.health > zombie.maxHealth) {
+        zombie.health = 100;
     }
-    document.getElementById('hits').innerText = `${hits}`
-    document.getElementById('health').innerText = `${health}%`
-    document.getElementById('health').style = `width: ${health}%`
-    console.log(health)
+    if (zombie.health < 0) {
+        zombie.health = 0;
+    }
+    document.getElementById('hits').innerText = `${zombie.hits}`
+    document.getElementById('health').innerText = `${zombie.health}%`
+    document.getElementById('health').style = `width: ${zombie.health}%`
+    console.log(zombie.health)
 }
 
 update()
